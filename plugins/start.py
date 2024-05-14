cf315df5e9d7a9c5a0ea7067f94d787772bf151a
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-05-14 22:06:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -86,8 +86,8 @@ async def start_command(client: Client, message: Message):
             InlineKeyboardButton("ʜᴇʟᴘ​", callback_data="help")
         ],
         [
-            InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/Anime_Mayhem"),
-            InlineKeyboardButton("ᴏɴɢᴏɪɴɢ", url="https://t.me/Ongoing_Anime_Mayhem")
+            InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/Anime_City_official"),
+            InlineKeyboardButton("ᴏɴɢᴏɪɴɢ", url="https://t.me/narutoshipudden_official")
         ],
         [
             InlineKeyboardButton("💥ᴄʟᴏꜱᴇ💥", callback_data="close")
